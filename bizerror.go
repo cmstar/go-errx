6db65fed53cf1d28ea34b6abf7a485717456ab4b
cmstar <cosmo_sei@hotmail.com>
2021-10-21 22:20:15 +0800

    不再定义一些和 HTTP 状态有关联的错误码了。

--- bizerror.go
+++ bizerror.go
@@ -5,34 +5,30 @@ import "fmt"
 // ErrorCode 表示错误信息的状态码。0 表示无错误；其余均表示错误。
 type ErrorCode int
 
-// 预定义的状态码。1000以下基本抄 HTTP 状态码。
 const (
 	// 未定义。
 	ErrorCodeUndefined = -1
 
 	// 无错误。
-	ErrorCodeSuccess = 0 // 这个不抄 HTTP 的 200 。
-
-	// 不合规的请求数据。
-	ErrorCodeBadRequest = 400
-
-	// 发生内部错误。
-	ErrorCodeInternalError = 500
+	ErrorCodeSuccess = 0
 )
 
-// 表示一个业务预定义错误。当遇到业务预定义错误时，方法可返回或 panic 此错误。
+// BizError 实现 error 。表示一个业务预定义错误。当遇到业务预定义错误时，方法可返回或 panic 此错误。
 type BizError struct {
-	// 对应 ApiResponse.Code 。
+	// Code 表示错误码。
 	Code ErrorCode
 
-	// 对应 ApiResponse.Message 。
+	// Message 是描述错误的信息。
 	Message string
 
 	// Inner 记录引起此错误的内部错误。
-	// 当认为一个 error 可以被作为 BizError 对待时，可创建 BizError 并设置此字段。
+	// 当一个 error 可以被作为 BizError 对待时，可创建 BizError 并设置此字段。
 	// 若没有内部错误，为 nil 。
 	Inner error
 }
+
+// Ensure implementation.
+var _ error = BizError{}
 
 // Error 实现 error 接口，返回 BizError 的数据，格式为： (Code) Message 。
 func (e BizError) Error() string {
