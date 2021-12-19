c0a9589d07463e6a9786e9bcd25d71425d8cd0a4
cmstar <cosmo_sei@hotmail.com>
2021-12-19 15:09:28 +0800

    移除 ErrorCode 定义，直接用 int ，简化使用。

--- bizerror.go
+++ bizerror.go
@@ -5,25 +5,13 @@ import (
 	"strings"
 )
 
-// ErrorCode 表示错误信息的状态码。0 表示无错误；其余均表示错误。
-type ErrorCode int
-
-const (
-	// 未定义。
-	ErrorCodeUndefined = -1
-
-	// 无错误。
-	ErrorCodeSuccess = 0
-)
-
 // BizError 是一个 error 。增加了错误码等属性，可更精确的表示一个预定义的错误。
 // BizError 实现 StackfulError ，可以携带调用栈信息。
 type BizError interface {
 	StackfulError
 
-	// Code 表示错误码。
-	// 在 webapi 中，对应 ApiResponse.Code 。
-	Code() ErrorCode
+	// Code 表示错误码。通常 0 表示没有错误。
+	Code() int
 
 	// Message 返回错误的描述信息。
 	Message() string
@@ -38,15 +26,15 @@ type BizError interface {
 type bizErr struct {
 	ErrorCause
 	ErrorStack
-	code    ErrorCode
+	code    int
 	message string
 }
 
 // Ensure implementation.
 var _ BizError = (*bizErr)(nil)
 
-// Code 返回错误码。通常 0 是一个保留值，表示没有错误，不用于任何错误。
-func (e *bizErr) Code() ErrorCode {
+// Code 返回错误码。通常 0 表示没有错误。
+func (e *bizErr) Code() int {
 	return e.code
 }
 
@@ -59,17 +47,17 @@ func (e *bizErr) Message() string {
 func (e *bizErr) Error() string {
 	var b strings.Builder
 	b.WriteRune('(')
-	b.WriteString(strconv.Itoa(int(e.code)))
+	b.WriteString(strconv.Itoa(e.code))
 	b.WriteString(") ")
 	b.WriteString(e.message)
 	res := b.String()
 	return res
 }
 
-// NewBizError 创建一个 BizError 。
-// 可通过 cause 指定引发此错误的错误，可以为 nil 。
-// 此方法创建的 BizError 会包含方法调用栈信息，通过 debug.Stack() 获取。
-func NewBizError(code ErrorCode, message string, cause error) BizError {
+// NewBizError 创建一个 BizError ，给定错误码、错误信息和引起此错误的错误。
+// cause 指定引发此错误的错误，可以为 nil 。
+// 此方法创建的 BizError 会包含方法调用栈信息，通过 runtime.Stack(buf, false) 获取。
+func NewBizError(code int, message string, cause error) BizError {
 	bizErr := &bizErr{
 		code:       code,
 		message:    message,
@@ -79,10 +67,10 @@ func NewBizError(code ErrorCode, message string, cause error) BizError {
 	return bizErr
 }
 
-// NewBizErrorWithoutStack 创建一个 BizError 。
-// 可通过 cause 指定引发此错误的错误，可以为 nil 。
+// NewBizErrorWithoutStack 创建一个 BizError ，给定错误码、错误信息和引起此错误的错误。
+// cause 指定引发此错误的错误，可以为 nil 。
 // 和 NewBizError() 类似，但不带调用栈信息， BizError.Stack() 返回空字符串。
-func NewBizErrorWithoutStack(code ErrorCode, message string, cause error) BizError {
+func NewBizErrorWithoutStack(code int, message string, cause error) BizError {
 	bizErr := &bizErr{
 		code:       code,
 		message:    message,
